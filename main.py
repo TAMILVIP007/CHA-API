4f53efbfeb51eb978af1396aa45aa4868ec2aaaa
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-05-23 00:43:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from requests.api import request
 import json
 import re
-https://icap.iconiq.ai/talk?&botkey=icH-VVd4uNBhjUid30-xM9QhnvAaVS3wVKA3L8w2mmspQ-hoUB3ZK153sEG3MX-Z8bKchASVLAo~&channel=7&sessionid=482070240&client_name=uuiprod-un18e6d73c-user-19422&id=true
+
 def chatbot(message):
     ai = {
         "input": message,
@@ -22,7 +22,7 @@ def chatbot(message):
 app = FastAPI()
 @app.get('/')
 def root(request: Request):
-    return {"root": request.url.hostname/docs}
+    return {"root": request.url.hostname}
 
 @app.get('/chatbot')
 async def chatbot(message: str):
